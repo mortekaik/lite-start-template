efc75d0b01818c4a9c5fbbd92ff3c63147681771
mortekaik <astral.eskimo@gmail.com>
2019-10-18 17:30:22 +0300

    added a package imagemin-pngquant

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -3,18 +3,28 @@
 import { paths } from '../paths';
 import gulp from 'gulp';
 import imagemin from 'gulp-imagemin';
+import imageminPngquant from 'imagemin-pngquant';
 import debug from 'gulp-debug';
 
 export default function images() {
 	return gulp.src(paths.images.src)
 		.pipe(imagemin([
 			imagemin.gifsicle({interlaced: true}),
-			imagemin.optipng({optimizationLevel: 5}),
 			imagemin.jpegtran({progressive: true}),
+			imageminPngquant({
+				speed: 5,
+				quality: [ 0.6, 0.8 ]
+			}),
 			imagemin.svgo({
 				plugins: [
-					{removeViewBox: false},
-					{cleanupIDs: false}
+					{ removeViewBox: false },
+					{ removeUnusedNS: false },
+					{ removeUselessStrokeAndFill: false },
+					{ cleanupIDs: false },
+					{ removeComments: true },
+					{ removeEmptyAttrs: true },
+					{ removeEmptyText: true },
+					{ collapseGroups: true }
 				]
 			})
 		]))
